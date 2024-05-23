5e9b610851538db4e38089d34082bc73c4124ac8
Andreina14-hub <84418323+Andreina14-hub@users.noreply.github.com>
2024-05-23 21:33:55 +0200

    page

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  Base: "https://andreina14-hub.github.io/pokeapi-project/",
 });
